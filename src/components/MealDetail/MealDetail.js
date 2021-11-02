9e816bb670776eb6aa6c49c117dfb89e49a70d34
manavjasani <manavjasani12@gmail.com>
2021-11-02 14:39:49 +0530

    Add meal-detail features

--- src/components/MealDetail/MealDetail.js
+++ src/components/MealDetail/MealDetail.js
@@ -1,24 +1,37 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import "./MealDetail.css";
 
 function MealDetail() {
-    
+    const [mealDetail, setMealDetail] = useState([]);
     const params = useParams();
     const {id} = params;
-    console.log(id);
+
     useEffect(() => {
         axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
             .then(res => {
-                console.log(res)
-
+                const resData = res.data.meals
+                console.log(resData)
+                setMealDetail(resData)
             })
             .catch(err => console.log(err));
     }, [id])
 
     return (
         <div>
-            hello
+            {mealDetail.map((item) => {
+                 return <div className="meal-detail" key={item.idMeal}> 
+                            <ul className="meal-detail-desc">
+                                <li>Category : {item.strCategory}</li>
+                                <li>Ingredients: <a href='#'>{item.strIngredient1}</a>, <a href='#'>{item.strIngredient2}</a></li>
+                                <li>Item : {item.strTags}</li>
+                            </ul>
+                            <div className="meal-image-container">
+                                <img src={item.strMealThumb} alt={item.category} />    
+                            </div>
+                        </div>
+            })}
         </div>
     )
 }
